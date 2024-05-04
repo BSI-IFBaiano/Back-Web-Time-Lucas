7ae5448ec97cde22b687677f894e6caeb0370dd8
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2024-05-04 11:16:33 -0300

    feat: adiciona classes para gerenciamento de erros

---
+++ src/main/java/com/web/desenvolvimento/edusphere/config/ExceptionEntityHandler.java
@@ -0,0 +1,25 @@
+package com.web.desenvolvimento.edusphere.config;
+
+import com.web.desenvolvimento.edusphere.domain.user.exceptions.UserInvalidUpdateException;
+import com.web.desenvolvimento.edusphere.domain.user.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class ExceptionEntityHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserInvalidUpdateException.class)
+    public ResponseEntity<String> handleInvalidUpdate(UserInvalidUpdateException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+}
