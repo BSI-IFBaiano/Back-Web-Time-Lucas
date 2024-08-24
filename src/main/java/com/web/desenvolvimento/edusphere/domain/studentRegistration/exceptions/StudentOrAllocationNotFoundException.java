69c062cc8144724c0b80cdd1acbf9274659af728
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2024-08-24 00:26:49 -0300

    feat: cria erro personalizado caso não encontre os codigos de estudante e/ou alocacao

---
+++ src/main/java/com/web/desenvolvimento/edusphere/domain/studentRegistration/exceptions/StudentOrAllocationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.web.desenvolvimento.edusphere.domain.studentRegistration.exceptions;
+
+public class StudentOrAllocationNotFoundException extends RuntimeException {
+    public StudentOrAllocationNotFoundException(String message) {
+        super(message);
+    }
+}
